8cfe6ecd77312e4ef8e5de1bf7f6fd53cccfb5ca
GOISQUE-SARRAZIN CORENTIN <corentin.goisque-sarrazin@etu.unistra.fr>
2025-01-02 23:18:05 +0100

    feat(trend): finalisation des données cibles

--- googleTrend.py
+++ googleTrend.py
@@ -1,5 +1,12 @@
 from pytrends.request import TrendReq
 import pandas as pd
+from datetime import datetime
+import os
+
+pd.set_option('future.no_silent_downcasting', True)
+def nowdate():
+    return datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
 
 def trendGlobal():
     # Connexion à PyTrends
@@ -10,8 +17,8 @@ def trendGlobal():
 
     trend_names = global_trending[0].tolist()
 
-    for trend in trend_names : 
-        print(trend)
+    # for trend in trend_names : 
+    #     print(trend)
 
     return trend_names
 
@@ -32,10 +39,25 @@ def interestSubject7days():
 
     all_interest = pd.concat([interest_over_time1, interest_over_time2], axis=1)
 
-    print(all_interest)
+    #print(all_interest)
 
-    all_interest.to_csv('trends_week.csv', index=True)
+    if 'isPartial' in all_interest.columns:
+        all_interest = all_interest.drop(columns=['isPartial'])
 
+    # Calculer la somme pour chaque colonne (chaque sujet)
+    total_interest = all_interest.sum().sort_values(ascending=False)
+    #print(total_interest)
+    return total_interest
+
+
+    # output_dir = 'csvTrend'
+    # if not os.path.exists(output_dir):
+    #     os.makedirs(output_dir)
+
+    # file_name = os.path.join(output_dir, f'trends_week_{nowdate()}.csv')
+    # all_interest.to_csv(file_name, index=True)
+
+    # print(f"Fichier enregistré sous le nom : {file_name}")
 
 
 
